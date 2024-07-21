1f82d2fe07a3117fd60875d9b1f1d256d77cfc0d
Sukumar Satyen <125384989+suksat@users.noreply.github.com>
2024-07-21 09:15:14 +0530

    Heap Sort

---
+++ HeapSort.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+class HeapSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter comma-separated numbers:");
+
+        String input = scanner.nextLine();
+        String[] stringArr = input.split(",");
+        int[] arr = new int[stringArr.length];
+
+        for (int i = 0; i < stringArr.length; i++) {
+            arr[i] = Integer.parseInt(stringArr[i].trim());
+        }
+
+        heapSort(arr);
+
+        printArray(arr);
+        scanner.close();
+    }
+}
