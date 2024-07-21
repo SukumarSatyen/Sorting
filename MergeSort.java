481a6bc1ca0f48ec1a228ccf80bfef12086d165d
Sukumar Satyen <125384989+suksat@users.noreply.github.com>
2024-07-21 09:08:51 +0530

    Merge Sort

---
+++ MergeSort.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+class MergeSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int leftArr[] = new int[n1];
+        int rightArr[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void sort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter comma-separated numbers:");
+
+        String input = scanner.nextLine();
+        String[] stringArr = input.split(",");
+        int[] arr = new int[stringArr.length];
+
+        for (int i = 0; i < stringArr.length; i++) {
+            arr[i] = Integer.parseInt(stringArr[i].trim());
+        }
+
+        sort(arr, 0, arr.length - 1);
+
+        printArray(arr);
+        scanner.close();
+    }
+}
